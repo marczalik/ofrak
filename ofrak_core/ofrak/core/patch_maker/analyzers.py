588e6efd590b99ae4d9bae3b860eb6b51cc98c7c
Marc Zalik <marc.zalik@gmail.com>
2023-02-01 19:53:43 -0500

    Tag ComplexBlocks as LinkableSymbols and copy attributes

---
+++ ofrak_core/ofrak/core/patch_maker/analyzers.py
@@ -0,0 +1,21 @@
+from ofrak.core.patch_maker.linkable_symbol import LinkableSymbol, LinkableSymbolType
+
+from ofrak.component.analyzer import Analyzer
+from ofrak.core.complex_block import ComplexBlock
+from ofrak.resource import Resource
+
+
+class ComplexBlockSymbolAnalyzer(Analyzer[None, LinkableSymbol]):
+    targets = (ComplexBlock,)
+    outputs = (LinkableSymbol,)
+
+    async def analyze(self, resource: Resource, config: None) -> LinkableSymbol:
+        cb = await resource.view_as(ComplexBlock)
+        cb_mode = await cb.get_mode()
+
+        return LinkableSymbol(
+            cb.virtual_address,
+            cb.name,
+            LinkableSymbolType.FUNC,
+            cb_mode,
+        )
