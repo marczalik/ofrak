23e1973a4084887cc8ac52a5ef06c3c2422e4ee9
Marc Zalik <marc.zalik@gmail.com>
2023-04-27 17:01:20 -0400

    Modify backoff function

--- ofrak_chatgpt/ofrak_chatgpt/chatgpt.py
+++ ofrak_chatgpt/ofrak_chatgpt/chatgpt.py
@@ -1,9 +1,8 @@
-import openai
 import os
-import random
 import time
 
 from dataclasses import dataclass
+from openai.error import RateLimitError
 from typing import Optional
 
 from ofrak.model.component_model import ComponentConfig
@@ -28,35 +27,32 @@ def retry_with_exponential_backoff(
     func,
     initial_delay: float = 1,
     exponential_base: float = 2,
-    jitter: bool = True,
     max_retries: int = 10,
-    errors: tuple = (openai.error.RateLimitError,),
+    errors: tuple = (RateLimitError,),
 ):
     """Retry a function with exponential backoff."""
 
     def wrapper(*args, **kwargs):
-        # Initialize variables
+        if exponential_base < 1:
+            raise Exception(
+                f"Backoff must exponentially increase, {exponential_base} must be greater than or equal to 1."
+            )
         num_retries = 0
         delay = initial_delay
 
         # Loop until a successful response or max_retries is hit or an exception is raised
-        while True:
+        while num_retries < max_retries:
             try:
                 return func(*args, **kwargs)
 
             # Retry on specified errors
             except errors as e:
-                # Increment retries
                 num_retries += 1
 
-                # Check if max retries has been reached
-                if num_retries > max_retries:
+                if num_retries >= max_retries:
                     raise Exception(f"Maximum number of retries ({max_retries}) exceeded.")
 
-                # Increment the delay
-                delay *= exponential_base * (1 + jitter * random.random())
-
-                # Sleep for the delay
+                delay *= exponential_base
                 time.sleep(delay)
 
             # Raise exceptions for any errors not specified
